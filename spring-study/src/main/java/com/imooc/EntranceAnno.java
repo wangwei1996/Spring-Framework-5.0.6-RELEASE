145782428ee2fb5a90dfb9413200623682a528b8
Wei.Wang <1849553578@qq.com>
2021-03-04 22:00:30 +0800

    feat>spring>接下来的学习任务

--- spring-study/src/main/java/com/imooc/EntranceAnno.java
+++ spring-study/src/main/java/com/imooc/EntranceAnno.java
@@ -4,6 +4,7 @@ import com.imooc.controller.HelloController;
 import com.imooc.controller.HiController;
 import com.imooc.controller.WelcomeController;
 import com.imooc.services.WelcomeService;
+import org.springframework.cglib.core.DebuggingClassWriter;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,6 +19,12 @@ public class EntranceAnno {
 	 * 通过注解来初始化IOC
 	 */
 	public static void main(String[] args) {
+		// 获取程序运行的过程中产生的代理类(分别代表CGLIB代理和JDK动态代理)
+		System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY,
+				"/home/wei/workspace/SOURCE_CODE/Spring-Framework-v5.0.6.release/spring-proxy");
+		System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles",
+				"true");
+
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(EntranceAnno.class);
 
 		String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
@@ -28,7 +35,7 @@ public class EntranceAnno {
 		WelcomeService welcomeService = (WelcomeService) applicationContext.getBean("welcomeServiceImpl");
 		WelcomeController welcomeController = applicationContext.getBean(WelcomeController.class);
 
-		//welcomeService.sayHello("I am create by annotation");
+		welcomeService.sayHello("I am create by annotation");
 
 
 		HiController hiController = (HiController) applicationContext.getBean("hiController");
